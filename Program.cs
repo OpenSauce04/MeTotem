abc3132f2b8ce8a8f75428f94627c2d7529c4e2d
OpenSauce04 <danieljamesbradbury@hotmail.com>
2021-01-29 14:20:51 +0000

    Added text

--- Program.cs
+++ Program.cs
@@ -4,9 +4,36 @@ namespace MeTotem
 {
 	class Program
 	{
+		public static string userName;
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello World!");
+			Console.WriteLine("MeTotem v0");
+			Console.WriteLine("---------------------\n");
+
+			Console.Write("Enter the desired username: ");
+			userName = Console.ReadLine();
+
+			Console.Write("Checking username...");
+
+			Console.WriteLine("done");
+
+			Console.Write("Getting body texture...");
+
+			Console.WriteLine("done");
+
+			Console.Write("Getting head texture...");
+
+			Console.WriteLine("done");
+
+			Console.Write("Personalising manifest.json...");
+
+			Console.WriteLine("done");
+
+			Console.Write("Building pack...");
+
+			Console.WriteLine("done");
+
+			Console.WriteLine("Finished!");
 		}
 	}
 }
